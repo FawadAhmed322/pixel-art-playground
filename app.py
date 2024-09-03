67cdf97de5eaa3caab26c737fcac21c3fe465d44
Fawad <fawadahmed322@gmail.com>
2024-09-03 04:27:07 +0200

    set debug to false

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def handle_cell_update(data):
         emit('error', {'message': 'Board not found'})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0')
